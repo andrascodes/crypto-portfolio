1f55f1f03ea8608db5a50df59cf01d2618411ee4
Andras Szucs <andrew0szucs@gmail.com>
2021-05-05 20:12:00 +0200

    Add ts-node-dev and express

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-async function main() {
-    console.log("Hello World");
-}
+import express, { json } from "express";
 
-main();
+const app = express();
+app.use(json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello, TypeScript!" });
+});
+
+app.listen(3005, () => {
+  console.log("🚀 Server started on http://localhost:3005");
+});
