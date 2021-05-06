47d7c2d363a18b2beef303f1a0a54e54bdbb40d5
Andras Szucs <andrew0szucs@gmail.com>
2021-05-06 23:53:30 +0200

    Fix PORT in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv-safe";
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+const APP_URL = process.env.APP_URL;
+
 const app = express();
 app.use(json());
 
@@ -12,6 +15,6 @@ app.get("/healthcheck", (req, res) => {
   });
 });
 
-app.listen(3005, () => {
-  console.log("🚀 Server started on http://localhost:3005");
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on ${APP_URL}:${PORT}`);
 });
